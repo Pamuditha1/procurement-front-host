5a4773ccd99eb5d56927b4a71867e85356e9ee46
Pamuditha Rajapaksha <pamuditharajapaksha@gmail.com>
2022-01-09 16:21:32 +0530

    api config

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,9 @@
 const PORT = 3003;
-const PRODUCTION_API = "https://procurement-app-back.herokuapp.com/api";
+const PRODUCTION_API =
+  process.env.API || "https://procurement-app-back.herokuapp.com/api";
 const DEVELOPMENT_API = `http://localhost:${PORT}/api`;
 
 console.error("env", process.env.NODE_ENV);
 
 export const api =
   process.env.NODE_ENV === "development" ? DEVELOPMENT_API : PRODUCTION_API;
-
-//"https://procurement-app-back.herokuapp.com/api";
-//`http://localhost:${port}/api`
