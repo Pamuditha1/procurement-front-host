2c3bd071784ad8c3083cc4c925dc7dc10cdd1027
AnushikaNethsara <anushikanethsara@gmail.com>
2022-09-27 13:01:10 +0530

    Added DOM Testing

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,18 +1,27 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import * as renderer from "react-test-renderer";
 import AdminViewPO from "../components/admin/documents/AdminViewPO";
+import CheckboxWithLabel from "../components/checkBox/CheckboxWithLabel";
 
 describe("AdminViewPO Compenent", () => {
+
+    // unmount and cleanup DOM after the test is finished.
     afterEach(() => {
         cleanup();
     });
 
+    test("test", () => {
+        expect(true).toBe(true);
+    })
+
+    //component testing
     test("Should render AdminViewPO component", () => {
         render(<AdminViewPO />);
         const element = screen.getByTestId("adminviewpo");
         expect(element).toBeInTheDocument();
     });
 
+    //snapshot testing
     test("Should matches DOM Snapshot", () => {
         const domTree = renderer.create(<AdminViewPO />).toJSON();
         expect(domTree).toMatchSnapshot();
@@ -24,7 +33,24 @@ describe("AdminViewPO Compenent", () => {
         expect(element).toHaveTextContent("Admin PO");
     });
 
-    test("test", () => {
-        expect(true).toBe(true);
-    })
-})
+    it.skip("Should render without crashing", () => {
+        render(<AdminViewPO />);
+        const element = screen.getByTestId("adminpo-heading");
+        expect(element).toHaveTextContent("Admin PO List");
+    });
+
+
+    //DOM Testing
+    it("CheckboxWithLabel changes the text after click", () => {
+
+        //checkbox which swaps between two labels:
+        const { queryByLabelText, getByLabelText } = render(
+            <CheckboxWithLabel labelOn="On" labelOff="Off" />,
+        );
+
+        expect(queryByLabelText(/off/i)).toBeTruthy();
+        fireEvent.click(getByLabelText(/off/i));
+        expect(queryByLabelText(/on/i)).toBeTruthy();
+    });
+
+});
